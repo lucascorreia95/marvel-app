ae070a0322dee2b20206ced36e40f8c942629074
Lucas Correia <lucascorreia-.-@live.com>
2019-05-16 19:41:11 -0300

    Updates

---
+++ precache-manifest.93cfc602fdda91f4e9ce494f0ee26ff9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "edff7232dcae38695205",
+    "url": "/marvel-app/static/css/main.93966222.chunk.css"
+  },
+  {
+    "revision": "edff7232dcae38695205",
+    "url": "/marvel-app/static/js/main.edff7232.chunk.js"
+  },
+  {
+    "revision": "4e342df3cb995d4d75eb",
+    "url": "/marvel-app/static/js/runtime~main.4e342df3.js"
+  },
+  {
+    "revision": "d3204ee4011ae4e0d00c",
+    "url": "/marvel-app/static/js/2.d3204ee4.chunk.js"
+  },
+  {
+    "revision": "91d7f6149a8abe97a444b28300a94400",
+    "url": "/marvel-app/static/media/MarvelLogo.91d7f614.svg"
+  },
+  {
+    "revision": "951975da73521bd6bd2a4284e22c43c5",
+    "url": "/marvel-app/static/media/iron-man.951975da.png"
+  },
+  {
+    "revision": "35ec9b563711bf120659a59afa4b7cdc",
+    "url": "/marvel-app/static/media/comic-cover.35ec9b56.jpg"
+  },
+  {
+    "revision": "78fcf3f651a2da3ccb2ae3a4535d821f",
+    "url": "/marvel-app/static/media/loading-img.78fcf3f6.gif"
+  },
+  {
+    "revision": "1d1ae4b624ba507ba1ea3bfa21978600",
+    "url": "/marvel-app/static/media/bg-header.1d1ae4b6.jpg"
+  },
+  {
+    "revision": "02bb4b2169de41813f1bde9f53a43534",
+    "url": "/marvel-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.31afa07dc580f35a34fb028564dc5de9.js"
+  "/marvel-app/precache-manifest.93cfc602fdda91f4e9ce494f0ee26ff9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/marvel-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
