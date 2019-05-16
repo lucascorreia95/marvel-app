d2607de11476b6fc4761bd10a2c95c144ca5da89
Lucas Correia <lucascorreia-.-@live.com>
2019-05-16 19:23:10 -0300

    Updates

---
+++ precache-manifest.31afa07dc580f35a34fb028564dc5de9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cbe2d0120078af7ab11",
+    "url": "/static/css/main.583f9b1a.chunk.css"
+  },
+  {
+    "revision": "3cbe2d0120078af7ab11",
+    "url": "/static/js/main.3cbe2d01.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d3204ee4011ae4e0d00c",
+    "url": "/static/js/2.d3204ee4.chunk.js"
+  },
+  {
+    "revision": "91d7f6149a8abe97a444b28300a94400",
+    "url": "/static/media/MarvelLogo.91d7f614.svg"
+  },
+  {
+    "revision": "951975da73521bd6bd2a4284e22c43c5",
+    "url": "/static/media/iron-man.951975da.png"
+  },
+  {
+    "revision": "35ec9b563711bf120659a59afa4b7cdc",
+    "url": "/static/media/comic-cover.35ec9b56.jpg"
+  },
+  {
+    "revision": "78fcf3f651a2da3ccb2ae3a4535d821f",
+    "url": "/static/media/loading-img.78fcf3f6.gif"
+  },
+  {
+    "revision": "1d1ae4b624ba507ba1ea3bfa21978600",
+    "url": "/static/media/bg-header.1d1ae4b6.jpg"
+  },
+  {
+    "revision": "b313e17210a608605638f0a14612285e",
+    "url": "/index.html"
+  }
+];
